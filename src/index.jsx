4afeac904ebaf02b929dc63e09b5c49a90719a7a
lea-bana <barreau.lea@gmail.com>
2022-10-04 12:30:41 +0200

    Modal components jsx jsdoc and styles

---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Exemple from "./exemple";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Exemple />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
